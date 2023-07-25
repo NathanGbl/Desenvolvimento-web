8d4b5b36efa5376f29069d851d5643912a724ac2
NathanGbl <nathangabriel.f.leite@gmail.com>
2023-07-24 21:10:44 -0300

    update

--- youtube/script/index.js
+++ youtube/script/index.js
@@ -1,26 +1,43 @@
 var search_icon = document.getElementById('search-icon')
-var search_bar = document.getElementById('search')
-var side_bar = document.getElementsByClassName('side-bar')
+var search_input = document.getElementById('search')
+var side_bar = document.querySelector('.side-bar')
 var button = document.getElementById('button')
+var img_search_img = document.querySelector('.img-search-img')
+var clear_icon = document.getElementById('clear')
 
 // Search Bar 
 
-search_bar.addEventListener('focus', () => {
+search_input.addEventListener('focus', () => {
     search_icon.style.display = 'block'
+    img_search_img.style.border = '1px solid #1c62b9'
+    img_search_img.style.box_shadow = 'inset 0 1px 2px rgba(0,0,0,0.3)'
 })
 
-search_bar.addEventListener('blur', () => {
+search_input.addEventListener('blur', () => {
     search_icon.style.display = 'none'
+    img_search_img.style.border = '1px solid rgb(63, 63, 63)'
 })
+
+if (search_input.length > 0) {
+    clear_icon.style.display = 'block'
+} else {
+    clear_icon.style.display = 'none'
+}
+
 
 //
 
 // Side bar
 
 button.addEventListener('click', () => {
-    if (side_bar.style.display == 'block') {
+    if (side_bar.style.display == 'flex') {
         side_bar.style.display = 'none'
     } else {
-        side_bar.style.display = 'flex' 
+        side_bar.style.display = 'flex'
     }
 })
+
+//     border: 1px solid #1c62b9;
+//     box-shadow: inset 0 1px 2px rgba(0,0,0,0.3);
+//     margin-left: 0px;
+//     padding: 2px 4px 2px 48px;
