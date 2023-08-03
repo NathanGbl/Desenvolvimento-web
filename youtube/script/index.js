13a453aa31f8ccac3b0109fc1ea2dfbe87533f08
NathanGbl <nathangabriel.f.leite@gmail.com>
2023-08-03 16:46:19 -0300

    update

--- youtube/script/index.js
+++ youtube/script/index.js
@@ -4,23 +4,23 @@ var side_bar = document.querySelector('.side-bar')
 var button = document.getElementById('button')
 var img_search_img = document.querySelector('.img-search-img')
 var clear_icon = document.getElementById('clear')
-var icon_input_icon = document.querySelector('icon-input-icon')
+var icon_input_icon = document.querySelector('.icon-input-icon')
 
 // Search Bar 
 search_icon.style.display = 'none'
+clear_icon.style.display = 'none'
 
 search_input.addEventListener('focus', () => {
     search_icon.style.display = 'block'
-    img_search_img.border_radius = 'none'
-    icon_input_icon.style.box_shadow = 'inset 0 1px 2px rgba(0,0,0,0.3)'
+    icon_input_icon.style.border = '1px solid #1c62b9'
 })
 
 search_input.addEventListener('blur', () => {
     search_icon.style.display = 'none'
+    icon_input_icon.style.border = '1px solid hsl(0, 0%, 18.82%)'
 })
 
-// clear_icon.style.display = 'none'
-// search_icon.style.display = 'none'
+
 
 //
 
